6e1d384b99395d55c7b3d367e592c3f1e0c3f092
weathon <tcs06358@163.com>
2019-03-28 21:56:42 -0500

    .........

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ with open("fin.dict","rb") as f:
 img0=Image.open("input.png").convert('1')
 
 #New Image
-img = Image.new( '1', (img0.size[0]*2,img0.size[1]*2),0) # create a new black image
+img = Image.new( '1', (img0.size[0]*2,img0.size[1]*2),255) # create a new black image
 pixels0 = img0.load() # create the pixel map
 pixels = img.load()
 
@@ -19,10 +19,16 @@ for i in range(img0.size[1]-1):
         for pix in value:
             p=int(pix)
             # print([(i-1)*2+myindex%4,(j-1)*2+myindex//4])
-            pixels[(i)*2+myindex%4,(j)*2+myindex//4]=max(p*255,pixels[(i)*2+myindex%4,(j)*2+myindex//4])
-            # print(p*255)
+            pixels[(i)*2+myindex%4,(j)*2+myindex//4]=min(p*255,pixels[(i)*2+myindex%4,(j)*2+myindex//4])
+            #Go through everyone? YES!
+            # pixels[(i)*2+myindex%4,(j)*2+myindex//4]=0
+
             myindex+=1
-        
+for i in range(img.size[1]-1): 
+    for j in range(img.size[0]-1):
+        if pixels[i,j]==255:
+            print((i,j))
+
 
 img.show()
 img.save("new.png")
